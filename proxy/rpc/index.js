5d607e507fde9f7b074ba1c74bd16d5c1b12dcb3
EC2 Default User <ec2-user@ip-172-31-16-146.us-east-2.compute.internal>
2020-01-26 16:27:28 +0000

    working rpc

--- proxy/rpc/index.js
+++ proxy/rpc/index.js
@@ -5,9 +5,20 @@ const fs = require('fs')
 var cors = require('cors')
 var app = express()
 
+const Web3 = require('web3')
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:46235"));
+
+const EthereumTx = require('ethereumjs-tx').Transaction
+const privateKey = Buffer.from(
+  '161a0889751bed7f5997c82233d2383073c9cfbfb6a7fa17f1f6c91864292412',
+  'hex',
+)
+
+let mnemonic = fs.readFileSync("mnemonic.txt")
 
 const { exec } = require('child_process');
-exec('ganache-cli -h 0.0.0.0 -p 46235 > ganache.log', (err, stdout, stderr) => {
+console.log("STARTING GANACHE...")
+exec('ganache-cli -h 0.0.0.0 -p 46235 -m "'+mnemonic+'" > ganache.log', (err, stdout, stderr) => {
   if (err) {
     //some err occurred
     console.error(err)
@@ -44,6 +55,50 @@ app.get('/', (req, res) => {
     });
     console.log("served!")
 })
+
+app.get('/faucet', (req, res) => {
+  console.log("faucet")
+  console.log(req.query)
+
+/*
+  const txParams = {
+    nonce: '0x00',
+    gasPrice: '0x09184e72a000',
+    gasLimit: '0x2710',
+    to: req.query.address,
+    value: '0x01',
+    data: '0x00',
+  }
+
+  // The second parameter is not necessary if these values are used
+  const tx = new EthereumTx(txParams)
+  tx.sign(privateKey)
+  const serializedTx = tx.serialize()
+
+  console.log("serializedTx",serializedTx)*/
+
+  web3.eth.getTransactionCount("0x7fc7e2a9564a8eaf0942907c6d323087fed5ea19", function (err, nonce) {
+
+    console.log("nonce:",nonce)
+    var tx =  new EthereumTx({
+      nonce: nonce,
+      gasPrice: web3.utils.toHex(web3.utils.toWei('20', 'gwei')),
+      gasLimit: 30000,
+      to: req.query.address,
+      value: 10000000000000000,
+      data: "0x",
+    });
+    tx.sign(Buffer.from(privateKey, 'hex'));
+
+    var raw = '0x' + tx.serialize().toString('hex');
+    web3.eth.sendSignedTransaction(raw, function (err, transactionHash) {
+      console.log(transactionHash);
+    });
+  });
+
+  res.send('0x0000')
+})
+
 
 https.createServer({
   key: fs.readFileSync('server.key'),
