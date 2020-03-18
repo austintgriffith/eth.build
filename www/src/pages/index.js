d842c74397def7c89c78d7afee660103421b4db5
Hunter Chang <hunter@madwiremedia.com>
2020-03-17 22:54:58 -0600

    adding www dir

---
+++ www/src/pages/index.js
@@ -0,0 +1,71 @@
+import { graphql, Link } from 'gatsby';
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const BlogIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title;
+  const posts = data.allMarkdownRemark.edges;
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+
+      <p className="mb-4">
+        <a className="btn btn-blue" href="#">
+          hello there
+        </a>
+      </p>
+
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug;
+        return (
+          <article key={node.fields.slug}>
+            <header>
+              <h3>
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt
+                }}
+              />
+            </section>
+          </article>
+        );
+      })}
+    </Layout>
+  );
+};
+
+export default BlogIndex;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`;
