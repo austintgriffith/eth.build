0aed72394aef40b0b77ca4e81d5c46a2885a7e49
Hunter Chang <hunter@madwiremedia.com>
2020-05-17 12:57:55 -0600

    homepage cleanup

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`
+        icon: `content/assets/emoji-icon.png`
       }
     },
     `gatsby-plugin-react-helmet`,
