dd0f1af438c4efb48afa69f7b2677f7685581de8
Aragar199 <avponce@hotmail.com>
2021-08-17 16:30:48 -0700

    clean up

--- src/nodes/Network/IPFSPublish.js
+++ src/nodes/Network/IPFSPublish.js
@@ -23,7 +23,7 @@ IPFSPub.prototype.onAdded = async function() {
      Addresses: {
        Swarm: ['/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
        '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star']
-     }
+     },
      Bootstrap: []
    }
   })
