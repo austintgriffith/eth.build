8e0e8202a33cfc04fe7c4dbd96ec22ebc7bb8028
Aragar199 <avponce@hotmail.com>
2021-08-25 09:46:05 -0700

    update cl options

--- geth/index.js
+++ geth/index.js
@@ -5,7 +5,7 @@ const { exec } = require('child_process');
 console.log("Starting up geth node...")
 try{
 
-  exec('./go-ethereum/build/bin/geth --rpcport 48545 --allow-insecure-unlock --cache=4096 --maxpeers=50 --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --rpcapi="db,eth,net,web3,personal,admin,debug,miner,txpool" >> ./geth.log 2>&1', (err, stdout, stderr) => {
+  exec('./go-ethereum/build/bin/geth --http.port 48545 --allow-insecure-unlock --cache=4096 --maxpeers=50 --http --http.addr "0.0.0.0" --http.corsdomain "*" --http.api="eth,net,web3,personal,admin,debug,miner,txpool" >> ./geth.log 2>&1', (err, stdout, stderr) => {
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
   })
